67ae436bca0dc7bcb82a90d10d161e9d4d5ec075
kjxbyz <47768002+kjxbyz@users.noreply.github.com>
2025-11-20 20:58:28 +0800

    fix: moved to Qiazo

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "insco-inc/scoop-bucket:main"
+    [String]$upstream = "qiazo/scoop-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
